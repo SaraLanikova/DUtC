74ba2b3e721acfd7c8e72c9325960e61f30b96db
slvstr1 <slvstr@gmail.com>
2024-06-11 01:13:13 +0200

    using player.participant to store values over rounds

--- dishonesty_experiment/die_under_the_cup/__init__.py
+++ dishonesty_experiment/die_under_the_cup/__init__.py
@@ -3,6 +3,7 @@ from otree.api import (
 )
 import random
 
+
 class Constants(BaseConstants):
     name_in_url = 'die_under_the_cup'
     players_per_group = None
@@ -10,16 +11,22 @@ class Constants(BaseConstants):
     endowment_loss_frame = c(6)  # Endowment for loss frame
 
 class Subsession(BaseSubsession):
+    # pass
     def creating_session(self):
-        for player in self.get_players():
-            player.actual_number = random.randint(1, 6)
-            if self.round_number == 1:
-                player.frame = random.choice(['Gain', 'Loss'])
-            # else:
-            #     player.frame = player.in_round(1).frame
+        pass
+    #     for player in self.get_players():
+    #         player.actual_number = random.randint(1, 6)
+    #         if self.round_number == 1:
+    #             # Assign values only in the first round
+    #             player.frame = random.choice(['Gain', 'Loss'])
+    #         else:
+    #             # Copy values from the first round in subsequent rounds
+    #             player.frame = player.in_round(1).frame
+
 
 class Group(BaseGroup):
     pass
+
 
 class Player(BasePlayer):
     round_number = models.IntegerField()
@@ -31,57 +38,57 @@ class Player(BasePlayer):
     actual_number = models.IntegerField()
     selected_for_payoff = models.BooleanField(initial=False)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.actual_number = random.randint(1, 6)
-        if not self.frame:
-            self.frame = random.choice(['Gain', 'Loss'])
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.actual_number = random.randint(1, 6)
 
-    def set_payoff(player):
-        if player.frame == 'Gain':
-            player.payoff = c(player.reported_number if player.reported_number != 6 else 0)
-        elif player.frame == 'Loss':
-            player.payoff = c(Constants.endowment_loss_frame - (player.reported_number if player.reported_number != 6 else 0))
+
+    def set_payoff(self):
+        if self.frame == 'Gain':
+            self.payoff = c(self.reported_number if self.reported_number != 6 else 0)
+        elif self.frame == 'Loss':
+            self.payoff = c(Constants.endowment_loss_frame - (self.reported_number if self.reported_number != 6 else 0))
+
 
 class Introduction(Page):
     def is_displayed(player):
+        if player.round_number == 1:
+            participant = player.participant
+            participant.frame = random.choice(['Gain', 'Loss'])
+            player.frame=participant.frame
+        else:
+            player.frame = player.participant.frame
         return player.round_number == 1
 
-    def before_next_page(self, timeout_happened):
-        subsession = self.subsession
-        for player in subsession.get_players():
-            player.actual_number = random.randint(1, 6)
-            player.actual_number = player.actual_number
-            break
+    def before_next_page(player: Player, timeout_happened):
+        pass
+
+
+    @staticmethod
+    def vars_for_template(player):
+        return {
+                'player_participant_frame': player.frame}
 
 class RollDice(Page):
     form_model = 'player'
     form_fields = ['reported_number']
 
-    # @staticmethod
-    # def vars_for_template(player):
-    #     return {
-    #         'actual_number': player.actual_number,
-    #         'round_number': player.round_number,
-    #         'total_rounds': Constants.num_rounds
-    #     }
-
-###################################################################
-    # svk:
     @staticmethod
-    def vars_for_template(self): # renaming player into self is not necessary. Otree names it as "player"? If yes, then keep "player".
+    def vars_for_template(player):
         return {
-            'actual_number': self.actual_number,
-            'round_number': self.round_number,
+            'actual_number': player.actual_number,
+            'round_number': player.round_number,
             'total_rounds': Constants.num_rounds,
-            'frame': self.frame
+            'frame_debug': player.participant.frame,
         }
-###################################################################
+
 
     def before_next_page(player, timeout_happened):
         player.set_payoff()
 
+
     def is_displayed(player):
+        player.actual_number = random.randint(1, 6)
         return player.round_number <= Constants.num_rounds  # Display the page for the specified number of rounds
 
 class Results(Page):
